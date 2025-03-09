6fa854a5f7e7ffb1778d760e0b2b96f2ca8395da
keithwu <594340838@qq.com>
2025-03-09 22:14:08 +0800

    fix cors

---
+++ sz-service/sz-service-admin/src/main/java/com/sz/configuration/WebConfig.java
@@ -0,0 +1,16 @@
+package com.sz.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")  // 生产环境替换为前端域名
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
